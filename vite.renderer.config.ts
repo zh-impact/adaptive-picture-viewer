06b3ef7dd2526755d79e5568becaa8c1dc61b1a6
Zhi.Huang <zh.impact@outlook.com>
2025-11-05 05:38:58 +0800

    chore: add temporary type ignore for Tailwind CSS import

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig(async () => {
+  // 三重禁忌 ignore, TODO: investigate typescript and nodejs config
+  // eslint-disable-next-line
+  // @ts-ignore
   // eslint-disable-next-line import/no-unresolved
   const tailwind = await import('@tailwindcss/vite');
 
